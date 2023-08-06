f17dce16b42d38368627b49193c1ac0ee92e312b
byga <gaby.shima12@gmail.com>
2023-08-06 20:09:45 -0300

    Adaptamos la implementación goto para que contemple estados

--- 12-histograma/histograma-2-goto.c
+++ 12-histograma/histograma-2-goto.c
@@ -1,15 +1,20 @@
-#ifndef HISTOGRAMA_2
-#define HISTOGRAMA_2
 #include <stdio.h>
 #include <stdlib.h>
 #include "graficador.h"
+#include "histograma.h"
 #ifndef MAX_WORD_LENGTH
 #define MAX_WORD_LENGTH 10
 #endif
 
 void withGoTo(FILE *stream)
 {
-	unsigned c, len = 0;
+	typedef enum
+	{
+		In,
+		Out,
+	} State;
+	State s = Out;
+	unsigned c, ncw = 0; // numero de caracteres de la palabra
 	unsigned arrayLengths[MAX_WORD_LENGTH + 1] = {0};
 	system("cls");
 	printf("Longitud máxima de palabra: %d\n", MAX_WORD_LENGTH);
@@ -18,16 +23,41 @@ read:
 	c = getc(stream);
 	if (c == EOF)
 		goto end;
-	if (c == ' ' || c == '\n' || c == '\t')
+
+	if (s == Out)
 	{
-		arrayLengths[len >= MAX_WORD_LENGTH ? MAX_WORD_LENGTH : len - 1]++;
-		len = 0;
+		if (c == ' ' || c == '\n' || c == '\t')
+		{
+			s = Out;
+			goto read;
+		}
+		else
+		{
+			s = In;
+			ncw = 0;
+			goto read;
+		}
 	}
-	else
-		len++;
-	goto read;
+	else if (s == In)
+	{
+		if (c == ' ' || c == '\n' || c == '\t')
+		{
+			if (ncw > MAX_WORD_LENGTH)
+				arrayLengths[MAX_WORD_LENGTH]++;
+			else
+				arrayLengths[ncw]++;
+			ncw = 0;
+			s = Out;
+			goto read;
+		}
+		else
+		{
+			s = In;
+			ncw++;
+			goto read;
+		}
+	}
 end:
 	printArray(arrayLengths, MAX_WORD_LENGTH);
 	return;
 }
-#endif
