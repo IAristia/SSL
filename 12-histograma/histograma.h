4cd610cb9f664325cae6fa83e604b95809e1e3a6
byga <gaby.shima12@gmail.com>
2023-08-06 20:37:18 -0300

    Añado contrato histograma.h

---
+++ 12-histograma/histograma.h
@@ -0,0 +1,10 @@
+#ifndef HISTOGRAMA_H
+#define HISTOGRAMA_H
+#include <stdio.h>
+
+unsigned withEnumSwitch(FILE *stream);
+void withGoTo(FILE *stream);
+unsigned withRec(FILE *stream);
+void withX(FILE *stream);
+
+#endif
