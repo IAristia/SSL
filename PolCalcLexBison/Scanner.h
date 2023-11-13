49c887d4b3157d94ebd582e991089963dae3a347
Iñaki <iaristia@frba.utn.edu.ar>
2023-11-13 17:03:27 -0300

    Flex y Bison
    Co-authored-by: Damian Ramirez <damianagustinrz@gmail.com>

---
+++ PolCalcLexBison/Scanner.h
@@ -0,0 +1,14 @@
+#ifndef SCANNER_H
+#define SCANNER_H
+
+enum TokenType
+{
+    NUMBER,
+    ADDITION = '+',
+    MULTIPLICATION = '*',
+    SUBTRACTION = '-',
+    DIVISION = '/',
+    POPRESULT = '\n',
+};
+
+#endif /* SCANNER_H*/
